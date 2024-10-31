5d988a223840d947dbd7a6b739a79041dfc784ff
Fedex95 <fedex95@outlook.es>
2024-10-31 11:15:07 -0500

    skye

--- src/main/java/com/tienda/kpback/Entity/Products.java
+++ src/main/java/com/tienda/kpback/Entity/Products.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.io.Serial;
 import java.io.Serializable;
 
 @Entity
@@ -16,6 +18,7 @@ import java.io.Serializable;
 @Setter
 
 public class Products implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
