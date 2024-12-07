8f7868dd4ae7f179b576a7415ddf26c619300c50
Fedex95 <fedex95@outlook.es>
2024-12-06 23:24:37 -0500

    back2.0

--- src/main/java/com/tienda/kpback/Repository/PedidoRep.java
+++
@@ -1,9 +0,0 @@
-package com.tienda.kpback.Repository;
-
-import com.tienda.kpback.Entity.PedidoEnt;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PedidoRep extends JpaRepository<PedidoEnt, Integer> {
-}
--- src/main/java/com/tienda/kpback/Repository/UsuarioRep.java
+++
@@ -1,9 +0,0 @@
-package com.tienda.kpback.Repository;
-
-import com.tienda.kpback.Entity.UsuarioEnt;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRep extends JpaRepository<UsuarioEnt, Integer> {
-}
