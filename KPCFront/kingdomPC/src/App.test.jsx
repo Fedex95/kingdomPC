80090bd915f9e01805a4456b6abbbff336f05de1
Fedex95 <fedex95@outlook.es>
2025-06-28 14:47:13 -0500

    o.O

--- KPCFront/kingdomPC/src/App.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders login when not authenticated', () => {
-  localStorage.clear();
-  render(<App />);
-  expect(screen.getByPlaceholderText(/usuario/i)).toBeInTheDocument();
-});
