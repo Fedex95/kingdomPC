176fded6e6ef61c14e78e09f6f33d3af5a0ab6f5
Fedex95 <fedex95@outlook.es>
2025-10-12 19:08:49 -0500

    bdd und nginx

--- KPCBack/kingdomPC/src/main/java/com/tienda/kpback/Repository/CartItemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.tienda.kpback.Repository;
-import com.tienda.kpback.Entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
--- KPCBack/kingdomPC/src/main/java/com/tienda/kpback/Repository/NotificacionesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tienda.kpback.Repository;
-
-import com.tienda.kpback.Entity.Notificaciones;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificacionesRepository extends JpaRepository<Notificaciones, Long>{
-}
