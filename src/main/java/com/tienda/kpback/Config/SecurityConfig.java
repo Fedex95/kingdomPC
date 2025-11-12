55dcce55f155683388804dc42883c15e6c48712e
Fedex95 <fedex95@outlook.es>
2025-11-12 00:50:14 -0500

    cd

--- src/main/java/com/tienda/kpback/Config/SecurityConfig.java
+++ src/main/java/com/tienda/kpback/Config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                     "/swagger-ui/**",
                     "/swagger-ui.html"
                 ).permitAll()
+                .requestMatchers("/api/auth/**").permitAll() 
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
