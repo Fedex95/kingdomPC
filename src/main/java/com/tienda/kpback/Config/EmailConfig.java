f26c015cd0317c2640882b7d632914e952f132bc
Fedex95 <fedex95@outlook.es>
2025-11-22 17:21:14 -0500

    adding user components

--- src/main/java/com/tienda/kpback/Config/EmailConfig.java
+++ src/main/java/com/tienda/kpback/Config/EmailConfig.java
@@ -1,5 +1,6 @@
 package com.tienda.kpback.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,19 +11,31 @@ import java.util.Properties;
 @Configuration
 public class EmailConfig {
 
+    @Value("${spring.mail.host}")
+    private String mailHost;
+
+    @Value("${spring.mail.port}")
+    private int mailPort;
+
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+
+    @Value("${spring.mail.password}")
+    private String mailPassword;
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        mailSender.setUsername("amandatets777@gmail.com");
-        mailSender.setPassword("bmhi qhxg pjrw nnqy"); 
+        mailSender.setHost(mailHost);
+        mailSender.setPort(mailPort);
+        mailSender.setUsername(mailUsername);
+        mailSender.setPassword(mailPassword);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");  
+        props.put("mail.debug", "true");
 
         return mailSender;
     }
