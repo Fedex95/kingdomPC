31d9b90fa23911c7a34d310792631672e043487e
Fedex95 <fedex95@outlook.es>
2025-11-22 14:17:48 -0500

    first

--- src/main/java/com/tienda/kpback/KpbackApplication.java
+++ src/main/java/com/tienda/kpback/KpbackApplication.java
@@ -2,35 +2,14 @@ package com.tienda.kpback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 
-/**
- * Aplicación principal de Kingdom PC Backend.
- */
-@OpenAPIDefinition(
-    info = @Info(
-        title = "Kingdom PC API",
-        version = "1.0",
-        description = "API para la aplicación Kingdom PC"
-    )
-)
-@SecurityScheme(
-    name = "bearerAuth",
-    type = SecuritySchemeType.HTTP,
-    scheme = "bearer",
-    bearerFormat = "JWT"
-)
+
 @SpringBootApplication
-public class KpbackApplication {
+public final class KpbackApplication {
 
-    /**
-     * Inicia la aplicación Spring Boot.
-     *
-     * @param args argumentos de la línea de comandos.
-     */
+    private KpbackApplication() {
+    }
+
     public static void main(final String[] args) {
         SpringApplication.run(KpbackApplication.class, args);
     }
