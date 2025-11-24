4e1ef6f69da927ee05c1ad5fd64573950c36704f
Fedex95 <fedex95@outlook.es>
2025-11-24 05:12:41 -0500

    hands up

--- src/main/java/com/tienda/kpback/Config/WebConfig.java
+++ src/main/java/com/tienda/kpback/Config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://172.18.0.2:80", "http://localhost:3000", "http://172.18.0.2") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization", "X-Requested-With", "Accept")
                 .allowCredentials(true);
